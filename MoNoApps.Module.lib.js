bbb83f1eb99cc2a0a6f651dd25f598a06380b845
Daniel Martinez <dmartz86@gmail.com>
2016-07-31 07:30:57 -0500

    Update MoNoApps.Module.lib.js

--- MoNoApps.Module.lib.js
+++ MoNoApps.Module.lib.js
@@ -6,5 +6,5 @@
     .service('DB', $M.Core.Api.DB)
     .service('Messenger', $M.Core.Api.Messenger)
     .controller('Game', $M.Core.Api.Game)
-    .controller('List', ['DB', $M.Core.Api.List]);
+    .controller('List', $M.Core.Api.List);
 })(angular.module, monoapps);
