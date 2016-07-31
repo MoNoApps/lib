a79b41f68721a8c845f2f9eab405377cbd6cd28e
Daniel Martinez <dmartz86@gmail.com>
2016-07-31 13:17:14 -0500

    Update MoNoApps.Messenger.lib.js

--- MoNoApps.Messenger.lib.js
+++ MoNoApps.Messenger.lib.js
@@ -1,5 +1,20 @@
-const MoNoApps = {
-  Messenger: class Messenger {
-    constructor() { }
+(($W, $M) => {
+  const Dependencies = [];
+
+  class Messenger {
+    constructor() {
+      // return $W.io();
+      return {
+        on(channel, callback) {
+          callback();
+        },
+        emmit(channel, message) {
+          console.log(channel, message);
+        }
+      };
+    }
   }
-}
+
+  $M.Core.Api.Messenger = Dependencies.concat(Messenger);
+
+})(window, MoNoApps);
